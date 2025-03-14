9fbbcdc985d8e6c7180c3f14de569d2b2473e39e
Firsdv <firsdv8@gmail.com>
2025-03-14 21:58:04 +0300

    Решено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean(Cat.class);
+        Cat bean3 = (Cat) applicationContext.getBean(Cat.class);
+
+
+        System.out.println(bean==bean1);
+        System.out.println(bean2==bean3);
+
+
     }
 }
